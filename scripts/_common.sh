2cc753eabbd6d95022ca3cd1833e4c730c33d2d8
ericgaspar <junk.eg@free.fr>
2021-05-01 12:12:35 +0200

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,7 +7,9 @@
 YNH_PHP_VERSION="7.3"
 
 # dependencies used by the app
-pkg_dependencies="php{YNH_PHP_VERSION}-cli php{YNH_PHP_VERSION}-common php{YNH_PHP_VERSION}-intl php{YNH_PHP_VERSION}-json git"
+extra_php_dependencies="php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-common php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-json"
+
+pkg_dependencies="git"
 
 #=================================================
 # PERSONAL HELPERS
