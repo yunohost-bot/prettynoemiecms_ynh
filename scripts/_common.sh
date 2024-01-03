c3186becc53fb7ee14f71c7d42dc6fc6f3e7ea9b
Félix Piédallu <felix@piedallu.me>
2024-01-03 21:39:54 +0100

    Port to manifestv2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,11 +4,6 @@
 # COMMON VARIABLES
 #=================================================
 
-YNH_PHP_VERSION="7.3"
-
-# dependencies used by the app
-pkg_dependencies="git php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-common php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-json"
-
 #=================================================
 # PERSONAL HELPERS
 #=================================================
